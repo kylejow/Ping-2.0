83b91c49d074562c3d6464bdf79edd150e162070
kylejow <kylejow8@gmail.com>
2023-03-03 22:05:50 -0800

    test interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #https://learn.sparkfun.com/tutorials/graph-sensor-data-with-python-and-matplotlib/update-a-graph-in-real-time
 
-import datetime as dt
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from pythonping import ping
@@ -15,6 +15,11 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 xs = []
 ys = []
+stop =False
+def interrupt():
+    time.sleep(3)
+    global stop
+    stop = True
 
 def animate(i, xs, ys, host, start):
     xs.append(str(time.time() - start) + 's')
@@ -27,6 +32,14 @@ def animate(i, xs, ys, host, start):
     plt.subplots_adjust(bottom=0.30)
     plt.title('Pinging ' + host)
     plt.ylabel('Latency (ms)')
+    if stop == True:
+        plt.close()
+t1 = threading.Thread(target=interrupt)
+t1.start()
 
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys, '8.8.8.8', time.time()), interval=100)
 plt.show()
+
+
+
+t1.join()
